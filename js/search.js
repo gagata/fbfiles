85ae95dea56360dda8c56b60b60302b30ac6bf91
Milosz Swizdor <m.swizdor@gmail.com>
2015-03-14 16:03:38 +0100

    Created content directory

--- js/search.js
+++ js/search.js
@@ -1,48 +1,14 @@
-function filter() {
-    var query = $('#search_box').val();
-    var regex = new RegExp(query, 'i');
-
-    $('.folder').remove();
-    var newList = new Array();
-
-    var groups = JSON.parse(localStorage.getItem('groups'));
-    for (var groupId in groups) {
-        var group = groups[groupId];
-        if (group['name'].match(regex) || filter == null) {
-            var nameTag = $('<div>', {class: 'name'});
-            nameTag.html(group['name']);
-
-            var updateTag = $('<div>', {class: 'date'});
-            updateTag.html(group['date']);
-
-            var idTag = $('<div>', {class: 'folder', id: group['id']});
-            var iconTag = $('<div>', {class: 'icon'});
-
-            var newGroup = idTag
-                .append(iconTag)
-                .append(nameTag)
-                .append(updateTag);
-            
-            // Stars go to the beginning, the rest to the end
-            if (group['starred']) {
-                newList.unshift(newGroup);
-            } else {
-                newList.push(newGroup);
-            }
-        }
-    }
-
-    $('.grid').append(newList.join(''));
-}
-
-function escape_specials(text) {
+/* Escapes special characters from the provided string. */
+function escapeSpecials(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
-function filter2() {
+/* Hides / shows elements of class 'item',
+   according to the query inside the search box. */ 
+function filter() {
     var query = $('#search_box').val();
-    var escaped_query = escape_specials(query);
-    var regex = new RegExp(escaped_query, 'i');
+    var escapedQuery = escapeSpecials(query);
+    var regex = new RegExp(escapedQuery, 'i');
 
     $('.item').each(function() {
         if (query == null ||
@@ -56,7 +22,5 @@ function filter2() {
 }
 
 $(document).ready(function() {
-    $('#search_box').keyup(filter2);
+    $('#search_box').keyup(filter);
 });
-
-
