3ab6e900c9b5da108ba8cc16f10320138f1c1eef
Milosz Swizdor <m.swizdor@gmail.com>
2015-03-14 16:13:32 +0100

    Appended search functions to groups_main.js

--- js/groups_main.js
+++ js/groups_main.js
@@ -8,6 +8,8 @@ function groups_main() {
         getGroups();
     });
     $(".back_to_root").remove();
+
+    $('#search_box').keyup(filter);
 }
 
 function getGroups() {
@@ -85,4 +87,28 @@ function present_folders(folders) {
         }
         
     }
+}
+
+/* Escapes special characters from the provided string. */
+function escapeSpecials(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+/* Shows the elements of class 'item' that contain the
+   search query either in their 'name' or 'post' field,
+   and hides the remaining ones. */ 
+function filter() {
+    var query = $('#search_box').val();
+    var escapedQuery = escapeSpecials(query);
+    var regex = new RegExp(escapedQuery, 'i');
+
+    $('.item').each(function() {
+        if (query == null ||
+            ($(this).find('.name').length > 0 && $(this).find('.name').html().match(regex)) ||
+            ($(this).find('.post').length > 0 && $(this).find('.post').html().match(regex))) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
 }
